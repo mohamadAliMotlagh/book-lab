0f1eeab1f103226d413c2b1a667f784757f4e816
hosein.motlagh <ali.highboy@yahoo.com>
2017-06-28 03:30:45 +0430

    a4 added
    a4 added dada

--- app/src/main/java/bookslab/android/com/booklab/MainActivity.java
+++ app/src/main/java/bookslab/android/com/booklab/MainActivity.java
@@ -2,6 +2,7 @@ package bookslab.android.com.booklab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("aa4a4a4a4a4a4","sssa4a4a4a4a4a4a4a4a4");
     }
 }
